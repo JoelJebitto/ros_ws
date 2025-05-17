3657104f1b9ec706ff25dae8cbf6f15d68ad1a8e
Joel Jebitto <joeljebitto@gmail.com>
2025-05-17 09:11:02 +0530

    added parameters to number publisher (cpp)

--- src/cpp_pubsub/src/number_publisher.cpp
+++ src/cpp_pubsub/src/number_publisher.cpp
@@ -1,5 +1,6 @@
 #include "example_interfaces/msg/int64.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include <chrono>
 #include <example_interfaces/msg/int64.hpp>
 #include <functional>
 #include <rclcpp/publisher.hpp>
@@ -8,35 +9,37 @@
 
 using namespace std::chrono_literals;
 
-
-class MyNode: public rclcpp::Node
-{
+class MyNode : public rclcpp::Node {
 public:
-    MyNode() : Node("number_publisher")
-    {
-        publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number_publisher", 10);        
-        timer_ = this->create_wall_timer(0.5s, std::bind(&MyNode::timer_callback, this));
-    }
+  MyNode() : Node("number_publisher") {
+    this->declare_parameter("number", 2);
+    this->declare_parameter("timer_period", 1.0);
+    number_ = this->get_parameter("number").as_int();
+    double timer_period_ = this->get_parameter("timer_period").as_double();
+    publisher_ = this->create_publisher<example_interfaces::msg::Int64>(
+        "number_publisher", 10);
+    timer_ =
+        this->create_wall_timer(std::chrono::duration<double>(timer_period_),
+                                std::bind(&MyNode::timer_callback, this));
+  }
 
 private:
-    void timer_callback()
-    {
-        auto msg = example_interfaces::msg::Int64();
-        msg.data = 7;
-        publisher_->publish(msg);
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
+  void timer_callback() {
+    auto msg = example_interfaces::msg::Int64();
+
+    number_ = this->get_parameter("number").as_int();
+    msg.data = number_;
+    publisher_->publish(msg);
+  }
+  int number_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
 };
 
-
-
-
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<MyNode>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MyNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
